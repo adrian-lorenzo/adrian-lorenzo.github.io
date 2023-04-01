e7e721898493e4cb6a9b485699f3446a52d21b99
Adrián Lorenzo Melián <adrian@theagilemonkeys.com>
2023-04-02 00:16:44 +0100

    Updates

---
+++ _app/immutable/chunks/1.f100e811.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.3525d549.js";export{t as component};
---
+++ _app/immutable/chunks/paths.fa34e9ed.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_zgnqt3)==null?void 0:s.base)??"/adrian-lorenzo.github.io";var t;const e=((t=globalThis.__sveltekit_zgnqt3)==null?void 0:t.assets)??a;export{e as a,a as b};
